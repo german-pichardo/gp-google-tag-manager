8558c09923ee7f505cbc9a3f17afeb0d026fabf7
German Pichardo <german-pichardo@users.noreply.github.com>
2018-05-11 11:22:09 +0200

    Update class-gp-google-tag-manager-admin.php

--- admin/class-gp-google-tag-manager-admin.php
+++ admin/class-gp-google-tag-manager-admin.php
@@ -4,8 +4,8 @@ if (!defined('ABSPATH')) {
     die;
 }
 
-if (!class_exists('GpGoogleTagManagerFront')) {
-    class GpGoogleTagManagerFront
+if (!class_exists('GpGoogleTagManagerAdmin')) {
+    class GpGoogleTagManagerAdmin
     {
         public $account_code;
 
@@ -14,43 +14,153 @@ if (!class_exists('GpGoogleTagManagerFront')) {
          */
         public function __construct()
         {
-            add_action('wp_head', [$this, 'snippet_head'], 1); // Wp hook to inject into head
-            add_action('google_tag_manager_snippet_body_action', [$this, 'snippet_body']);
+            // Add the page to the admin menu
+            add_action('admin_menu', [$this, 'add_plugin_page']);
+            // Register page options
+            add_action('admin_init', [$this, 'admin_page_init']);
+            // Add plugin settings link
+            add_filter('plugin_action_links', [$this, 'add_settings_link'], 10, 2);
+
             $this->account_code = (isset(get_option('gp_google_tag_manager_option_name')['gp_google_tag_manager_account_code_1']) && !empty(get_option('gp_google_tag_manager_option_name')['gp_google_tag_manager_account_code_1'])) ? get_option('gp_google_tag_manager_option_name')['gp_google_tag_manager_account_code_1'] : false;
         }
 
-        public function snippet_head()
+        /**
+         * Function that will add the options page under Setting Menu.
+         */
+        public function add_plugin_page()
+        {
+            // $page_title, $menu_title, $capability, $menu_slug, $callback_function
+            add_options_page(
+                __('Google Tag Manager Options'), // page_title
+                __('Gp Google Tag Manager'), // menu_title
+                'manage_options', // capability
+                'gp-google-tag-manager-options', // menu_slug
+                [$this, 'create_admin_page'] // function
+            );
+        }
+
+        /**
+         * Function that will display the options page.
+         */
+        public function create_admin_page()
+        { ?>
+            <div class="wrap">
+                <h2><?php _e('Google Tag Manager settings'); ?></h2>
+                <?php //settings_errors();
+                ?>
+
+                <form method="post" action="options.php">
+                    <?php
+                    settings_fields('gp_google_tag_manager_option_group');
+                    do_settings_sections('gp-google-tag-manager-admin-sections');
+                    submit_button();
+                    ?>
+                </form>
+            </div>
+        <?php }
+
+        public function admin_page_init()
+        {
+
+            // Register Settings
+            register_setting(
+                'gp_google_tag_manager_option_group', // option_group
+                'gp_google_tag_manager_option_name', // option_name
+                [$this, 'sanitize_values'] // sanitize_callback
+            );
+
+            // Add Section for option fields
+            add_settings_section(
+                'section_account_code', // id
+                '', // title
+                [$this, 'section_account_code_callback'], // callback
+                'gp-google-tag-manager-admin-sections' // page
+            );
+
+            // Add Section for labels
+            add_settings_section(
+                'gp_google_tag_manager_section_after_body', // id
+                '', // title
+                [$this, 'section_after_body_callback'], // callback
+                'gp-google-tag-manager-admin-sections' // page
+            );
+
+            // Add Slug Field
+            add_settings_field(
+                'gp_google_tag_manager_account_code_1', // id
+                __('GTM Code'), // title
+                [$this, 'account_code_1_callback'], // callback
+                'gp-google-tag-manager-admin-sections', // page
+                'section_account_code' // section
+            );
+        }
+
+        /**
+         * Section slug callback
+         */
+        public function section_account_code_callback()
+        {
+            echo "<hr><h2>" . __('Gtm code') . "</h2>";
+        }
+
+        /**
+         * Section label callback
+         */
+        public function section_after_body_callback()
         {
             if ($this->account_code) { ?>
-                <!-- Google Tag Manager -->
-                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','<?php echo $this->account_code;  ?>');</script>
-                <!-- End Google Tag Manager -->
-            <?php }
+                <hr><h2>After Body <small>(Optional)</small></h2>
+                <p>The second GTM code needs to be added manually in order to use Google Tag Manager for <u>Visitors Without JavaScript</u> </p>
+                <p class='description'>Copy the following snippet and paste it immediately <b><u>after</u> </b>the opening <code>body</code></p>
+                <code><?php echo htmlspecialchars("<?php if (function_exists('google_tag_manager_snippet_body')) {
+                        google_tag_manager_snippet_body();
+                    }; ?>"); ?></code>
+            <?php };
         }
 
-        // After body tag
-        public function snippet_body()
+        /**
+         * Functions that display the fields.
+         */
+        public function sanitize_values($input)
         {
-            if  ($this->account_code) { ?>
-                <!-- Google Tag Manager (noscript) -->
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $this->account_code;  ?>"
-                                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                <!-- End Google Tag Manager (noscript) -->
-            <?php }
+            $sanitary_values = [];
 
+            if (isset($input['gp_google_tag_manager_account_code_1'])) {
+                $sanitary_values['gp_google_tag_manager_account_code_1'] = sanitize_text_field($input['gp_google_tag_manager_account_code_1']);
+            }
+
+            return $sanitary_values;
         }
+
+        /**
+         * Fields individual callbacks
+         */
+
+        public function account_code_1_callback()
+        {
+            printf(
+                '<input type="text" placeholder="' . __('GTM-XXXXXXX', 'gp-google-tag-manager-text-domain') . '" class="regular-text" name="gp_google_tag_manager_option_name[gp_google_tag_manager_account_code_1]" value="%s" id="gp_google_tag_manager_account_code_1" >',
+                $this->account_code ? esc_attr($this->account_code) : ''
+            );
+        }
+
+        /**
+         * Functions that registers settings link on plugin description.
+         */
+        public function add_settings_link($links, $file)
+        {
+            $this_plugin = plugin_basename(__FILE__);
+
+            if (is_plugin_active($this_plugin) && $file == $this_plugin) {
+                $links[] = '<a href="' . admin_url('options-general.php?page=gp-google-tag-manager-options') . '">' . __('Settings', 'gp-google-tag-manager-text-domain') . '</a>';
+            }
+
+            return $links;
+
+        } // end add_settings_link
+
     }
 } // !class_exists
 
-if (!is_admin())
-    $gp_google_tag_manager = new GpGoogleTagManagerFront();
-
-// After Body action
-// Google Tag Manager for Visitors Without JavaScript
-function google_tag_manager_snippet_body() {
-    do_action('google_tag_manager_snippet_body_action');
-}
+if (is_admin())
+    $gp_google_tag_manager = new GpGoogleTagManagerAdmin();
